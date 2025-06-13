53233e7d701aae5e5afa23c56be86e666c647a93
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-06-13 11:37:28 +0600

    Add article retrieval by email and enhance article update and delete routes

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -21,6 +21,22 @@ export const getArticlesById = async (req, res) => {
   }
 };
 
+// Get articles by email
+export const getAllArticlesByEmail = async (req, res) => {
+  try {
+    const { email: creatorEmail } = req.params;
+    const articles = await ArticleModel.find({ creatorEmail });
+    if (!articles || articles.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No articles found for this email" });
+    }
+    res.status(200).json(articles);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 // Create a new articles
 export const addNewArticle = async (req, res) => {
   try {
@@ -29,5 +45,34 @@ export const addNewArticle = async (req, res) => {
     res.status(201).json(savedArticle);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+};
+
+// update article
+export const updateArticle = async (req, res) => {
+  try {
+    const updatedArticle = await ArticleModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedArticle) {
+      return res.status(404).json({ message: "Article not found" });
+    }
+    res.status(201).json(updatedArticle);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Remove a article
+export const deleteArticle = async (req, res) => {
+  try {
+    const article = await ArticleModel.findByIdAndDelete(req.params.id);
+    if (!article) return res.status(404).json({ message: "article not found" });
+    res.status(200).json(article);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -6,10 +6,19 @@ const router = express.Router();
 // Route to get all articles
 router.get("/articles", articleController.getAllArticles);
 
-// Route to get single articles by id
+// Route to get single article by id
 router.get("/articles/:id", articleController.getArticlesById);
 
 // Route to create article
 router.post("/articles", articleController.addNewArticle);
+
+// Route to get  articles by email
+router.get("/my-articles/:email", articleController.getAllArticlesByEmail);
+
+// Route to delete  articles by email
+router.delete("/my-articles/:id", articleController.deleteArticle);
+
+// Route to update
+router.put("/my-articles/:id", articleController.updateArticle);
 
 export default router;
