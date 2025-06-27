599a70c2cea9df74d695e0b0e10730a45d11923d
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-06-27 23:47:56 +0600

    Refactor firebase.js to comment out unused imports and improve readability

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
-import fs from "fs"; // To read files
-import path from "path"; // To build file paths
+// import fs from "fs"; // To read files
+// import path from "path"; // To build file paths
 import admin from "firebase-admin";
 
 // import { fileURLToPath } from "url"; // To get current file's path
@@ -10,9 +10,10 @@ import admin from "firebase-admin";
 
 // const serviceAccountPath = path.resolve(__dirname, "serviceAccountKey.json");
 
-const fileData = Buffer.from(process.env.FIREBASE_SERVICES_KEY).toString(
-  "utf8"
-);
+const fileData = Buffer.from(
+  process.env.FIREBASE_SERVICES_KEY,
+  "base64"
+).toString("utf8");
 
 const serviceAccount = JSON.parse(fileData);
 
