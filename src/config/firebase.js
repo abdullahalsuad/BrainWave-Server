af3cd60ed1242160d8c699f467662005f26165b4
Abdullah Al Suad <suadabdullahal@gmail.com>
2025-06-27 23:39:06 +0600

    Update .gitignore to include serviceAccountKey.json and refactor firebase.js to use environment variable for Firebase credentials

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,19 @@
 import fs from "fs"; // To read files
 import path from "path"; // To build file paths
-import { fileURLToPath } from "url"; // To get current file's path
 import admin from "firebase-admin";
 
+// import { fileURLToPath } from "url"; // To get current file's path
+
 // Step 2: Get current file and folder path
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const serviceAccountPath = path.resolve(__dirname, "serviceAccountKey.json");
+// const serviceAccountPath = path.resolve(__dirname, "serviceAccountKey.json");
 
-const fileData = fs.readFileSync(serviceAccountPath, "utf8");
+const fileData = Buffer.from(process.env.FIREBASE_SERVICES_KEY).toString(
+  "utf8"
+);
+
 const serviceAccount = JSON.parse(fileData);
 
 admin.initializeApp({
